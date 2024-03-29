8165177c54f6cc54fcfb368de04f9506a5989983
Ivan Dereviankin <ukrustacean@gmail.com>
2024-03-29 21:32:27 +0200

    Fix backend

--- back/main.js
+++ back/main.js
@@ -7,17 +7,19 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const PORT = 3000;
 const app = (0, express_1.default)();
+app.use(express_1.default.text());
 app.use((0, cors_1.default)());
-app.use(express_1.default.json());
 const DB = [];
-app.get("/notes", (_req, res) => {
+app.get('/notes', (_req, res) => {
     console.log(DB);
-    res.status(200).json({ notes: DB }).send();
+    res.status(200).json({ notes: DB });
+    res.end();
 });
-app.post("/note", (req, res) => {
-    console.log(req.body);
-    DB.push(req.body.note);
-    res.status(200).send();
+app.post('/note', (req, res) => {
+    const { note } = JSON.parse(req.body);
+    DB.push(note);
+    res.status(200);
+    res.end();
 });
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
--- back/main.ts
+++ back/main.ts
@@ -1,25 +1,27 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const PORT = 3000
-const app = express()
+const PORT = 3000;
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(express.text());
+app.use(cors());
 
-const DB: string[] = []
+const DB: string[] = [];
 
-app.get("/notes", (_req, res) => {
-    console.log(DB)
-    res.status(200).json({ notes: DB }).send()
-})
+app.get('/notes', (_req, res) => {
+    console.log(DB);
+    res.status(200).json({ notes: DB });
+    res.end();
+});
 
-app.post("/note", (req, res) => {
-    console.log(req.body)
-    DB.push(req.body.note)
-    res.status(200).send()
-})
+app.post('/note', (req, res) => {
+    const { note }: { note: string } = JSON.parse(req.body);
+    DB.push(note);
+    res.status(200);
+    res.end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-})
+    console.log(`Example app listening on port ${PORT}`);
+});
