15791cd28a4eb049a7fe0616ed2ebc7419f66f84
Ivan Dereviankin <ukrustacean@gmail.com>
2024-04-13 21:58:57 +0300

    Added MongoDB storage

--- back/main.js
+++ back/main.js
@@ -1,26 +1,56 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const PORT = 3000;
-const app = (0, express_1.default)();
-app.use(express_1.default.text());
-app.use((0, cors_1.default)());
-const DB = [];
-app.get('/notes', (_req, res) => {
-    console.log(DB);
-    res.status(200).json({ notes: DB });
-    res.end();
-});
-app.post('/note', (req, res) => {
-    const { note } = JSON.parse(req.body);
-    DB.push(note);
-    res.status(200);
-    res.end();
-});
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-});
+const mongoose_1 = __importDefault(require("mongoose"));
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const PORT = 3000;
+        const app = (0, express_1.default)();
+        yield mongoose_1.default.connect('mongodb+srv://ukrustacean:31E1cDx8YRDyxVZk@cluster0.bntnbmd.mongodb.net/?retryWrites=true&w=majority');
+        const noteSchema = new mongoose_1.default.Schema({ note: String });
+        const Note = mongoose_1.default.model('Note', noteSchema);
+        app.use(express_1.default.text());
+        app.use((0, cors_1.default)());
+        app.get('/notes', (_req, res) => __awaiter(this, void 0, void 0, function* () {
+            const notes = (yield Note.find({}))
+                .map(x => x.note);
+            console.log([...notes]);
+            res.status(200).json({ notes });
+            res.end();
+        }));
+        app.post('/note', (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const note = JSON.parse(req.body);
+            const newNote = new Note(note);
+            yield newNote.save();
+            console.log('Saved new note!');
+            res.status(200);
+            res.end();
+        }));
+        app.listen(PORT, () => {
+            console.log(`Example app listening on port ${PORT}`);
+        });
+        app.on('SIGTERM', () => {
+            mongoose_1.default.connection.close();
+        });
+        app.on('SIGKILL', () => {
+            mongoose_1.default.connection.close();
+        });
+        app.on('SIGINT', () => {
+            mongoose_1.default.connection.close();
+        });
+    });
+}
+main();
--- back/main.ts
+++ back/main.ts
@@ -1,27 +1,53 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
-const PORT = 3000;
-const app = express();
+async function main() {
+    const PORT = 3000;
+    const app = express();
 
-app.use(express.text());
-app.use(cors());
+    await mongoose.connect('mongodb+srv://ukrustacean:31E1cDx8YRDyxVZk@cluster0.bntnbmd.mongodb.net/?retryWrites=true&w=majority');
+    const noteSchema = new mongoose.Schema({ note: String });
+    const Note = mongoose.model('Note', noteSchema);
 
-const DB: string[] = [];
+    app.use(express.text());
+    app.use(cors());
 
-app.get('/notes', (_req, res) => {
-    console.log(DB);
-    res.status(200).json({ notes: DB });
-    res.end();
-});
+    app.get('/notes', async (_req, res) => {
+        const notes = (await Note.find({}))
+            .map(x => x.note!);
+        console.log([...notes]);
 
-app.post('/note', (req, res) => {
-    const { note }: { note: string } = JSON.parse(req.body);
-    DB.push(note);
-    res.status(200);
-    res.end();
-});
+        res.status(200).json({ notes });
+        res.end();
+    });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
-});
+    app.post('/note', async (req, res) => {
+        const note: { note: string } = JSON.parse(req.body);
+        const newNote = new Note(note);
+
+        await newNote.save();
+        console.log('Saved new note!');
+
+        res.status(200);
+        res.end();
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`);
+    });
+
+    app.on('SIGTERM', () => {
+        mongoose.connection.close();
+    });
+
+    app.on('SIGKILL', () => {
+        mongoose.connection.close();
+    });
+
+    app.on('SIGINT', () => {
+        mongoose.connection.close();
+    });
+}
+
+main();
